5ab795e241514e561bc668eaba325c242bad7fad
JiPengL <xuxiejp@163.com>
2020-09-16 15:12:28 +0800

    Add files via upload

---
+++ src/main/java/com/dync/service/TestService.java
@@ -0,0 +1,15 @@
+package com.dync.service;
+
+/**
+ * @author liujipeng
+ * @date 2020/9/11 11:48
+ * @mail liujipeng@cloud-er.com
+ * @desc ...
+ */
+public interface TestService {
+
+    Object getUserinfo();
+
+    Object getUser();
+
+}
